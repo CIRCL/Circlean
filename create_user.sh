5cfaba763840eb2d8b1088300d13e98b21aa02c3
Raphaël Vinot <raphael@vinot.info>
2013-11-20 15:40:29 +0100

    add sudoers file, ensuring no users are sudoers with no or default passwd

--- create_user.sh
+++ create_user.sh
@@ -2,8 +2,9 @@
 
 useradd -m kitten
 
-echo "Cmnd_Alias GROOMER_CMDS = /home/kitten/kitten_mount_src, \
-    /home/kitten/kitten_mount_dst, /home/kitten/kitten_umount" >> /etc/sudoers
-echo "kitten  ALL=(ALL) NOPASSWD: GROOMER_CMDS" >> /etc/sudoers
+# Useless: the sudoer file comes from the repository
+#echo "Cmnd_Alias GROOMER_CMDS = /home/kitten/kitten_mount_src, \
+#    /home/kitten/kitten_mount_dst, /home/kitten/kitten_umount" >> /etc/sudoers
+#echo "kitten  ALL=(ALL) NOPASSWD: GROOMER_CMDS" >> /etc/sudoers
 
 # /!\ REMOVE SUDO RIGHTS TO USER pi
