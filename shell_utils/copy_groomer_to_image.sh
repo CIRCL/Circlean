adef197f01b7d9e36fab09ef94a2c1d5307341d6
Dan Puttick <dputtick@users.noreply.github.com>
2017-01-31 16:44:59 -0500

    Rename fs_filecheck and include it under /circlean as /root_partition

--- shell_utils/copy_groomer_to_image.sh
+++ shell_utils/copy_groomer_to_image.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-cp /media/sf_ubuntu-shared/Circlean-Ubuntu/fs_filecheck/opt/groomer/init.sh /mnt/rpi/opt/groomer/init.sh
-cp /media/sf_ubuntu-shared/Circlean-Ubuntu/fs_filecheck/opt/groomer/groomer.sh /mnt/rpi/opt/groomer/groomer.sh
-cp /media/sf_ubuntu-shared/Circlean-Ubuntu/fs_filecheck/opt/groomer/config.sh /mnt/rpi/opt/groomer/config.sh
-cp /media/sf_ubuntu-shared/Circlean-Ubuntu/fs_filecheck/opt/groomer/mount_dest.sh /mnt/rpi/opt/groomer/mount_dest.sh
-# cp /media/sf_ubuntu-shared/Circlean-Ubuntu/fs_filecheck/opt/groomer/music.sh /mnt/rpi/opt/groomer/music.sh
-cp /media/sf_ubuntu-shared/Circlean-Ubuntu/fs_filecheck/etc/rc.local /mnt/rpi/etc/rc.local
-# cp /media/sf_ubuntu-shared/Circlean-Ubuntu/fs_filecheck/etc/pmount.allow /mnt/rpi/etc/pmount.allow
-# cp /media/sf_ubuntu-shared/Circlean-Ubuntu/fs_filecheck/etc/udev/rules.d/10-usb.rules /mnt/rpi/etc/udev/rules.d/10-usb.rules
+cp circlean/root_partition/opt/groomer/init.sh /mnt/rpi/opt/groomer/init.sh
+cp circlean/root_partition/opt/groomer/groomer.sh /mnt/rpi/opt/groomer/groomer.sh
+cp circlean/root_partition/opt/groomer/config.sh /mnt/rpi/opt/groomer/config.sh
+cp circlean/root_partition/opt/groomer/mount_dest.sh /mnt/rpi/opt/groomer/mount_dest.sh
+cp circlean/root_partition/etc/rc.local /mnt/rpi/etc/rc.local
+# cp circlean/root_partition/opt/groomer/music.sh /mnt/rpi/opt/groomer/music.sh
+# cp circlean/root_partition/etc/pmount.allow /mnt/rpi/etc/pmount.allow
+# cp circlean/root_partition/etc/udev/rules.d/10-usb.rules /mnt/rpi/etc/udev/rules.d/10-usb.rules
