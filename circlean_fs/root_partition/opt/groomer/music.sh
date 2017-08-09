fce66420c8479df57c3ce38ef7f985d5683a9674
Dan Puttick <dputtick@users.noreply.github.com>
2017-08-09 18:08:34 -0400

    Refactor music.sh to use functions

--- circlean_fs/root_partition/opt/groomer/music.sh
+++ circlean_fs/root_partition/opt/groomer/music.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
-set -e
-#set -x
-
-source ./config.sh
-
 killed(){
     echo 'Music stopped.'
 }
 
-trap killed EXIT TERM INT
-
-# Force output on analog
-amixer cset numid=3 1
-
-files=(${MUSIC_DIR}*)
-
-while true; do
-    # -id flags set interface to "dumb" and -qq silences most/all terminal output
-    $TIMIDITY -idqq ${files[RANDOM % ${#files[@]}]}
+run_timidity() {
+    # Force output on analog
+    amixer cset numid=3 1
+    files=(${MUSIC_DIR}*)
+    while true; do
+        # -id flags set interface to "dumb" and -qq silences most/all terminal output
+        "${TIMIDITY}" -idqq "${files[RANDOM % ${#files[@]}]}"
 done
+}
+
+main() {
+    set -eu  # exit when a line returns non-0 status, treat unset variables as errors
+    trap killed EXIT TERM INT  # run clean when the script ends or is interrupted
+    check_is_root
+    source ./config.sh  # get config values
+    run_timidity
+}
+
+main
